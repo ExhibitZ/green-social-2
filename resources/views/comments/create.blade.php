57b5bf5c02758ee1db947390a68fdb3e3a42e278
stanley <stanley@email.com>
2024-11-30 11:57:07 +0700

    change routing from resource controller into normal routing

---
+++ resources/views/comments/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous defer"></script>
+</head>
+<body>
+    <div class="container mt-3">
+        <h1 class="mb-3">Create a New Comment</h1>
+
+        @foreach ($errors->all() as $error)
+            <p class="text-danger">{{ $error }}</p>
+        @endforeach
+        <form action="{{ route('comments.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="message" class="form-label">Enter Message</label>
+                <textarea class="form-control" name="message" id="message" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,13 @@ Route::get('/', function () {
     return redirect()->route('posts.index');
 });
 
-Route::resource('posts', PostController::class)->only(['index', 'create', 'show', 'store', 'edit', 'update', 'destroy']);
+// Route::resource('posts', PostController::class)->only(['index', 'create', 'show', 'store', 'edit', 'update', 'destroy']);
+
+// posts routing
+Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class,'create'])->name('posts.create');
+Route::post('/posts', [PostController::class,'store'])->name('posts.store');
+Route::get('/posts/{postId}', [PostController::class,'show'])->name('posts.show');
+Route::get('/posts/{postId}/edit', [PostController::class,'edit'])->name('posts.edit');
+Route::put('/posts/{postId}', [PostController::class,'update'])->name('posts.update');
+Route::delete('/posts/{postId}', [PostController::class,'destroy'])->name('posts.destroy');
